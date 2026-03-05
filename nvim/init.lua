28e0c32a71dbc48c4087a335526208854c0b7ac2
ty <yueto@oocl.com>
2026-03-05 21:34:32 +0800

    edit

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Disable backup and swap files which trigger constant rclone syncing
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+
+-- Write directly to the file instead of a temporary "new" file
+vim.opt.backupcopy = "yes"
+
+vim.diagnostic.enable(false)
---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "folke/flash.nvim",
+    keys = {
+      { "s", mode = { "n", "x", "o" }, false },
+      { "S", mode = { "n", "x", "o" }, false },
+    },
+  },
+}
